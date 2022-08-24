09cf2f772e93e489b4ff86768c8b97a529121641
durga <durgarajendran.96@gmail.com>
2022-08-25 00:41:09 +0530

    Added api get request

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import Header from './components/Header';
 import Search  from './components/Search';
 import {useState} from 'react'
 
+const UNSPLASH_KEY=process.env.REACT_APP_UNSPLASH_KEY;
 
 // <h1> Image Gallery</h1>
 function App() {
@@ -12,8 +13,15 @@ function App() {
     e.preventDefault();
     //console.log(e.target[0].value);
     console.log(word);
+    fetch(`https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`)
+     .then((res) =>res.json())
+     .then((data)=>{
+      console.log(data);
+     })
+     .catch((err) => {console.log(e); })
+     setWord('');
     }
-    //console.log(word);
+    //console.log(word); console.log(process.env);
   return (
     <div className="App">
       <Header title="Image Gallery" />
